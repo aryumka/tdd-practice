16280c5c39fcc35bc0590aa9949f26383dee7c99
aryumka <yooooar@gmail.com>
2024-01-07 15:59:15 +0900

    Implement methods in accordance with tests

--- src/main/kotlin/aryumka/money/Money.kt
+++ src/main/kotlin/aryumka/money/Money.kt
@@ -11,6 +11,27 @@ class Money(
     return Money(this.currency, this.amount + money.amount)
   }
 
+  operator fun times(money: Money): Money {
+    if (this.currency != money.currency) {
+      throw IllegalArgumentException("currency is different")
+    }
+    return Money(this.currency, this.amount * money.amount)
+  }
+
+  operator fun div(money: Money): Money {
+    if (this.currency != money.currency) {
+      throw IllegalArgumentException("currency is different")
+    }
+    return Money(this.currency, this.amount / money.amount)
+  }
+
+  operator fun minus(money: Money): Money {
+    if (this.currency != money.currency) {
+      throw IllegalArgumentException("currency is different")
+    }
+    return Money(this.currency, this.amount - money.amount)
+  }
+
   override fun equals(other: Any?): Boolean {
     if (other is Money) {
       return this.currency == other.currency && this.amount == other.amount
