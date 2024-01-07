1872b2dbe7abfb66a685174cf5e778cc637894c3
aryumka <yooooar@gmail.com>
2024-01-07 15:36:21 +0900

    Implement plus and equals methods

--- src/main/kotlin/aryumka/money/Dollar.kt
+++ src/main/kotlin/aryumka/money/Dollar.kt
@@ -1,4 +1,16 @@
 package aryumka.money
 
-class Dollar {
+class Dollar(
+  val amount: Int
+) {
+  operator fun plus(dollar: Dollar): Dollar {
+    return Dollar(amount + dollar.amount)
+  }
+
+  override fun equals(other: Any?): Boolean {
+    if (other is Dollar) {
+      return amount == other.amount
+    }
+    return false
+  }
 }
