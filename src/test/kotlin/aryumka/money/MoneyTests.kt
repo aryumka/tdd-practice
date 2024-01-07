2b3ae5a1fe489b32457d7d5669db6449083285c1
aryumka <yooooar@gmail.com>
2024-01-07 16:10:53 +0900

    Add complex calculation test

--- src/test/kotlin/aryumka/money/MoneyTests.kt
+++ src/test/kotlin/aryumka/money/MoneyTests.kt
@@ -38,6 +38,17 @@ class MoneyTests: FunSpec({
     }
   }
 
+  context("Money can be complex calculated") {
+    test("Money(USD, 10) + Money(USD, 5) * Money(USD, 2) - Money(USD, 15) / Money(USD, 3) = Money(USD, 17)") {
+      val money1 = Money(USD, 10)
+      val money2 = Money(USD, 5)
+      val money3 = Money(USD, 2)
+      val money4 = Money(USD, 15)
+      val money5 = Money(USD, 5)
+      money1 + money2 * money3 - money4 / money5 shouldBe Money(USD, 17)
+    }
+  }
+
   context("Money can be compared by value") {
     test("Money(USD, 5) == Money(USD, 5)") {
       val money1 = Money(USD, 5)
