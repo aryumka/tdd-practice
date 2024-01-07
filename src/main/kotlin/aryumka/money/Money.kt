16733554c98d88c7a685d63380cbd28939ea74f3
aryumka <yooooar@gmail.com>
2024-01-07 16:02:31 +0900

    Implement comparison operator

--- src/main/kotlin/aryumka/money/Money.kt
+++ src/main/kotlin/aryumka/money/Money.kt
@@ -4,6 +4,7 @@ class Money(
   val currency: Currency,
   val amount: Int
 ) {
+  // Arithmetic operators
   operator fun plus(money: Money): Money {
     if (this.currency != money.currency) {
       throw IllegalArgumentException("currency is different")
@@ -32,6 +33,15 @@ class Money(
     return Money(this.currency, this.amount - money.amount)
   }
 
+  // Comparison operators
+  operator fun compareTo(money: Money): Int {
+    if (this.currency != money.currency) {
+      throw IllegalArgumentException("currency is different")
+    }
+    return this.amount.compareTo(money.amount)
+  }
+
+  // Equality operators
   override fun equals(other: Any?): Boolean {
     if (other is Money) {
       return this.currency == other.currency && this.amount == other.amount
